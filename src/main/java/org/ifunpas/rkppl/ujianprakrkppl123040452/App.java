a973e0fe0ab6a9fa3aef79cf8e2eb5b3d66aaa2f
604-10 <604-10@604-10.labif.local>
2014-05-22 15:37:51 +0700

    Commit pertama ujian RKPPL Maven

---
+++ src/main/java/org/ifunpas/rkppl/ujianprakrkppl123040452/App.java
@@ -0,0 +1,13 @@
+package org.ifunpas.rkppl.ujianprakrkppl123040452;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/ifunpas/rkppl/ujianprakrkppl123040452/Soal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.ujianprakrkppl123040452;
+
+/**
+ *
+ * @author 604-10
+ */
+public class Soal {
+    
+    public int pangkat (int bil, int pangkat){
+        int hasil = 0;
+        int n = 1;
+        while (n <= pangkat){
+            n++;
+            hasil *= bil;
+        }
+        return hasil;
+    }
+    
+    public int faktorial (int bil) {
+        int hasil = 1;
+        for (int i = 1; i<=bil; i++){
+            hasil *= i;
+        }
+        return hasil;
+    }
+    
+    public int perkalian(int bilA, int bilB){
+        return bilA*bilB;
+    }
+    
+}
